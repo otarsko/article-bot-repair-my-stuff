e97e3630aaf50ed2614fa6dd829f472f3ca53140
OTarasenko <oleks.tarasenko@gmail.com>
2016-10-27 22:40:41 +0300

    Updated help command.

--- src/command/help.js
+++ src/command/help.js
@@ -2,6 +2,7 @@
 
 export default class Help {
     handle(message, bot) {
-        return bot.sendMessage(message.from, 'Type /fix your_address_and_problem_description to submit request (e.g. /fix NY. TV got broken');
+        return bot.sendMessage(message.from, '/fix your_address_and_problem_description - to submit request (e.g. /fix NY. TV got broken)\n'
+            + '/myrequests - to get submitted request');
     }
 }
